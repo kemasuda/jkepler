1def3832e19efde4b8b264faea9b2763c1ae4e23
Hajime Kawahara <divrot@gmail.com>
2024-08-18 08:31:53 +0900

    added astrometry_test.py

--- src/jkepler/tests/read_testdata.py
+++ src/jkepler/tests/read_testdata.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pkg_resources
 import jax.numpy as jnp
+from astropy.time import Time
 
 def read_testdata_rv():
     """Read in test data for RVs
@@ -19,3 +20,16 @@ def read_testdata_rv():
     t -= tepoch
 
     return t, y, yerr, tepoch
+
+def read_testdata_astrometry():
+
+    test_astfile = pkg_resources.resource_filename("jkepler", "data/tests/mann19_astrometry.txt")
+    test_orbfile = pkg_resources.resource_filename("jkepler", "data/tests/mann19_orbit.txt")
+
+    data = pd.read_csv(test_astfile, delimiter='|', comment='#')
+    dates = [data.Date[i]+"T00:00:00" for i in range(len(data))]
+    times = Time(dates, format='isot', scale='utc').mjd
+    data['MJD'] = times
+    odata = pd.read_csv(test_orbfile, delimiter='|', comment='#')
+    data["Name"] = [n.strip(" ") for n in data.Name]
+    odata["Name"] = [n.strip(" ") for n in odata.Name]
---
+++ tests/unittests/astrometry/astrometry_test.py
@@ -0,0 +1,12 @@
+from jkepler.astrometry import xyzv_from_elements
+import pytest
+import numpy as np
+
+def test_xyzv_from_elements():
+    # Test case from astrometry.ipynb
+    assert False
+
+if __name__ == "__main__":
+    test_xyzv_from_elements()
+
+

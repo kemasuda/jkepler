58ea7870c680cd1eddcb7decb984477f48ecaefd
Hajime Kawahara <divrot@gmail.com>
2024-08-17 12:18:05 +0900

    adds pandas in requirement

--- tests/unittests/rv/rv_test.py
+++ tests/unittests/rv/rv_test.py
@@ -1,14 +1,27 @@
 import pytest
 from jkepler.rv import rv_unit_amplitude as getrv
 import pkg_resources
+import pandas as pd
+import jax.numpy as jnp
 
 def read_testdata_rv():
-    testdata = pkg_resources.resource_filename('jkepler', 'data/tests/v723mon_s12_rv.csv')
-    return testdata
+    """Read in test data for RVs
+
+    Returns:
+        _type_: pandas.DataFrame
+    """
+    test_rvfile = pkg_resources.resource_filename('jkepler', 'data/tests/v723mon_s12_rv.csv')
+    test_rv = pd.read_csv(test_rvfile)
+    return test_rv
 
 def test_getrv():
-    dat = read_testdata_rv()
-    print(dat)
+    rvdata = read_testdata_rv()
+    t, y, yerr = jnp.array(rvdata.BJD), jnp.array(rvdata.rv), jnp.array(rvdata.rv_err)
+    idx = t!=2454073.62965 # bad data
+    t, y, yerr = t[idx], y[idx], yerr[idx]
+    tepoch = t[0]
+    t -= tepoch
 
+    
 if __name__ == '__main__':
     test_getrv()

fd4a09091f35e860eb77f6c7f2eda7323c99807c
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2024-08-26 15:00:28 +0900

    flux loss for multiple wavelengths w/o vmap

--- src/jkepler/transit/transit.py
+++ src/jkepler/transit/transit.py
@@ -1,5 +1,5 @@
 
-__all__ = ["flux_loss_cosi", "flux_loss_b", "flux_loss_cosi_map", "flux_loss_b_map"]
+__all__ = ["flux_loss_cosi", "flux_loss_b", "flux_loss_cosi_map", "flux_loss_b_map", "flux_loss_cosi_multiwav", "flux_loss_b_multiwav"]
 
 import jax.numpy as jnp
 from jax import jit, vmap
@@ -56,6 +56,10 @@ def compute_flux_loss(rsky_over_rstar, rp_over_rstar, z_sign, u1, u2):
 
 @jit
 def flux_loss_cosi(t, t0, period, ecc, omega, cosi, a_over_rstar, rp_over_rstar, u1, u2):
+    """compute flux loss given cosi
+    works when t0, period, ..., rp_over_rstar are arrays of the same shapes as long as u1, u2 are scalers
+    if u1 and u2 are also arrays, use flux_loss_cosi_multiwav instead
+    """
     rsky_over_a, z_sign = rsky_over_a_t0(t, t0, period, ecc, omega, cosi)
     rsky_over_rstar = rsky_over_a * a_over_rstar
     return compute_flux_loss(rsky_over_rstar, rp_over_rstar*jnp.ones_like(t), z_sign, u1, u2)
@@ -63,11 +67,59 @@ def flux_loss_cosi(t, t0, period, ecc, omega, cosi, a_over_rstar, rp_over_rstar,
 
 @jit
 def flux_loss_b(t, t0, period, ecc, omega, b, a_over_rstar, rp_over_rstar, u1, u2):
-    """ at least rp_over_rstar needs to be an array like t """
+    """compute flux loss given b
+    works when t0, period, ..., rp_over_rstar are arrays of the same shapes as long as u1, u2 are scalers
+    if u1 and u2 are also arrays, use flux_loss_b_multiwav instead
+    """
     cosi = b_to_cosi(b, ecc, omega, a_over_rstar)
     return flux_loss_cosi(t, t0, period, ecc, omega, cosi, a_over_rstar, rp_over_rstar, u1, u2)
 
 
+@jit
+def flux_loss_cosi_multiwav(t, t0, period, ecc, omega, cosi, a_over_rstar, rp_over_rstar, u1, u2):
+    """compute flux loss mapping along the parameters other than time t
+    flux_loss_cosi works if u1 & u2 are common
+
+        Args:
+            t: 1d time array (N,)
+            t0: 1d array of time of inferior conjunction (Np,)
+            period, ecc, ... should all be 1d arrays of the same shape (Np,)
+
+        Returns:
+            flux loss computed at time t, shape (N,Np)
+
+    """
+    rsky_over_a, z_sign = rsky_over_a_t0(t[:,None], t0, period, ecc, omega, cosi) # (N,Np)
+    rsky_over_rstar = rsky_over_a * a_over_rstar[None,:]
+    rp_over_rstar = jnp.ones_like(rsky_over_rstar) * rp_over_rstar[None,:]
+
+    soln = ops.quad_solution_vector(rsky_over_rstar, rp_over_rstar) # (N,Np,3)
+    g = jnp.array([1. - u1 - 1.5*u2, u1 + 2*u2, -0.25*u2]) # (3,Np)
+    I0 = jnp.pi * (g[0] + 2 * g[1] / 3.)  # (Np,)
+    flux_loss = jnp.sum(soln*g.T[None,:,:], axis=2) / I0[None,:] - 1. # (N,Np)
+    flux_loss = jnp.where(z_sign > 0, flux_loss, 0)
+
+    return flux_loss
+
+
+@jit
+def flux_loss_b_multiwav(t, t0, period, ecc, omega, b, a_over_rstar, rp_over_rstar, u1, u2):
+    """compute flux loss mapping along the parameters other than time t, given b instead of cosi
+    flux_loss_b works if u1 & u2 are common
+    
+        Args:
+            t: 1d time array (N,)
+            t0: 1d array of time of inferior conjunction (Np,)
+            period, ecc, ... should all be 1d arrays of the same shape (Np,)
+
+        Returns:
+            flux loss computed at time t, shape (N, Np)
+
+    """
+    cosi = b_to_cosi(b, ecc, omega, a_over_rstar)
+    return flux_loss_cosi_multiwav(t, t0, period, ecc, omega, cosi, a_over_rstar, rp_over_rstar, u1, u2)
+
+'''Below are vmap versions of flux_loss_*_multiwav; obsolete'''
 # vmap version of rsky_over_a_t0
 rsky_over_a_t0_map = vmap(rsky_over_a_t0, (None,0,0,0,0,0), 0)
 
